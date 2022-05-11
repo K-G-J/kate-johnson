be3584c1f306cdc83d9ce7eebaaf46efaf0bec88
Kate Johnson <johnson.kat.g@gmail.com>
2022-05-11 00:16:43 -0400

    added latest projects

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,14 +1,24 @@
 module.exports = {
   portfolio: [
     {
-      name: 'Dashboard3',
+      name: 'CoinMarketCap Blockchain Clone',
       description:
-        'Web3 dashboard to view all user transactions, accounts, NFTs, and send crypto',
-      link: 'https://dashboard3-rho.vercel.app/',
-      video: 'https://youtu.be/SlghW9vYiK8',
-      image: 'dashboard3',
-      github: 'https://github.com/K-G-J/Dashboard3',
+        'A Web3 clone of CoinMarketCap using blockchain technology and Solidity contract backend',
+      link: 'https://youtu.be/13SdluC5_n0',
+      video: 'https://youtu.be/13SdluC5_n0',
+      image: 'cmc-clone',
+      github: 'https://github.com/K-G-J/coinmarketcap-clone-blockchain',
     },
+
+    {
+      name: 'Cryptocurrency Portfolio Calculator',
+      description: 'CoinGeko API and custom backend price calculation formula',
+      link: 'https://crypto-dex.herokuapp.com/',
+      video: 'https://youtu.be/cKy3Gu-WAb4',
+      image: 'crypto-calculator',
+      github: 'https://github.com/K-G-J/crypto-calculator',
+    },
+
     {
       name: 'Full Mint Website',
       description: 'Smart contract powered site for minting an NFT collection',
@@ -18,12 +28,38 @@ module.exports = {
       github: 'https://github.com/K-G-J/full-mint-website-front-end',
     },
     {
+      name: 'Decentralized Twitter Clone',
+      description: 'Mint profile NFTs, post tweets, ETH tip tweets of other users',
+      link: 'https://youtu.be/iy3TrL8yChc',
+      video: 'https://youtu.be/iy3TrL8yChc',
+      image: 'twitter-clone',
+      github: 'https://github.com/K-G-J/twitter-blockchain-clone',
+    },
+    {
+      name: 'Spooky Hollow NFT Markplace',
+      description:
+        'A fully functional NFT marketplace for minitng, buying, and trading NFTs',
+      link: 'https://youtu.be/HQo_A0LOlrg',
+      video: 'https://youtu.be/HQo_A0LOlrg',
+      image: 'spooky-hollow',
+      github: 'https://github.com/K-G-J/spooky-hollow',
+    },
+    {
       name: 'Krypt',
       description: 'A beautiful app for sending crypto and a fun GIF message',
       link: 'https://krypt-gif.com/',
       video: 'https://youtu.be/gPebC7jLgRE',
       image: 'krypt',
       github: 'https://github.com/K-G-J/krypt',
+    },
+    {
+      name: 'Dashboard3',
+      description:
+        'Web3 dashboard to view all user transactions, accounts, NFTs, and send crypto',
+      link: 'https://dashboard3-rho.vercel.app/',
+      video: 'https://youtu.be/SlghW9vYiK8',
+      image: 'dashboard3',
+      github: 'https://github.com/K-G-J/Dashboard3',
     },
     {
       name: 'NFT-y',
@@ -41,16 +77,6 @@ module.exports = {
       video: 'https://youtu.be/eVfqcp6XUu4',
       image: 'lazy-mint',
       github: 'https://github.com/K-G-J/nft-minter',
-    },
-
-    {
-      name: 'Spooky Hollow NFT Markplace',
-      description:
-        'A fully functional NFT marketplace for minitng, buying, and trading NFTs',
-      link: 'https://youtu.be/HQo_A0LOlrg',
-      video: 'https://youtu.be/HQo_A0LOlrg',
-      image: 'spooky-hollow',
-      github: 'https://github.com/K-G-J/spooky-hollow',
     },
   ],
 }
