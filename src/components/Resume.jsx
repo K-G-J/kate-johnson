54eb17873430c884dfcb8eda2b349f14c48b8590
Kate Johnson <johnson.kat.g@gmail.com>
2023-07-30 01:36:56 -0400

    Updated spacing in resume section

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -118,7 +118,7 @@ export default function Resume() {
                     <path d="M2 13.692V16a2 2 0 002 2h12a2 2 0 002-2v-2.308A24.974 24.974 0 0110 15c-2.796 0-5.487-.46-8-1.308z" />
                   </svg>
                 </div>
-                <h2 className="text-lg  font-medium title-font mb-6">
+                <h2 className="text-lg  font-medium title-font mb-10">
                   Experience
                 </h2>
                 <p className="leading-relaxed text-base font-bold">
@@ -246,7 +246,7 @@ export default function Resume() {
                     />
                   </svg>
                 </div>
-                <h2 className="text-lg font-medium title-font mb-6">
+                <h2 className="text-lg font-medium title-font mb-10">
                   Education
                 </h2>
                 <p className="leading-relaxed text-base font-bold">
